02f0a7632a8a8aa9884bd7d97f2dba7d5141dd50
Виктория Фирсанова <vifirsanova@gmail.com>
2023-02-02 23:50:32 +0300

    Update files

--- mail.php
+++ mail.php
@@ -1,11 +1,44 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "vifirsanova@gmail.com"; // my Email address
-    $from = $_POST['email']; // sender's Email address
-    $subject = "Form submission";
-    $message = $_POST['email'];
+<?php
 
-    $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
-    }
+#Receive user input
+$email_address = $_POST['email_address'];
+
+#Filter user input
+function filter_email_header($form_field) {  
+return preg_replace('/[nr|!/<>^$%*&]+/','',$form_field);
+}
+
+$email_address  = filter_email_header($email_address);
+
+#Send email
+$headers = "From: $email_address";
+$sent = mail('vifirsanova@gmail.com', 'Feedback Form Submission', $headers);
+
+#Thank user or notify them of a problem
+if ($sent) {
+
+?><html>
+<head>
+<title>Thank You</title>
+</head>
+<body>
+<h1>Thank You</h1>
+<p>Thank you for your feedback.</p>
+</body>
+</html>
+<?php
+
+} else {
+
+?><html>
+<head>
+<title>Something went wrong</title>
+</head>
+<body>
+<h1>Something went wrong</h1>
+<p>We could not send your feedback. Please try again.</p>
+</body>
+</html>
+<?php
+}
 ?>
