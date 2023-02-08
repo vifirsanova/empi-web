53241945bf5e5becd78fa8713ded60003bbccc1f
Виктория Фирсанова <vifirsanova@gmail.com>
2023-02-09 00:59:52 +0300

    dark mode finished

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+const checkbox = document.getElementById('checkbox');
+
+checkbox.addEventListener('change', ()=>{
+  document.body.classList.toggle('light');
+})
