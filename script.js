03fc5005e46f0e1a71e449be507b2dab0b3c0b98
Виктория Фирсанова <vifirsanova@gmail.com>
2023-03-19 21:12:39 +0300

    bug fix

--- script.js
+++ script.js
@@ -3,3 +3,20 @@ const checkbox = document.getElementById('checkbox');
 checkbox.addEventListener('change', ()=>{
   document.body.classList.toggle('light');
 })
+
+var back_top_button = document.getElementById("scroll_btn");
+
+window.onscroll = function() {scrollfunc()};
+
+function scrollfunc() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+	back_top_button.style.display = "block";
+  } else {
+	back_top_button.style.display = "none";
+  }
+}
+
+function Scrollback() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
